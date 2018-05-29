3692a2240b5af1770c8efa044c82b7e4fcfaa4c6
jrj92280 <jrj92280@gmail.com>
2018-05-29 15:18:56 -0400

    Add Heroku files

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask_runner import Manager
 
+from app import app
+
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
+
+# gunicorn -b 0.0.0.0:5000 app:app --log-file=- -R
