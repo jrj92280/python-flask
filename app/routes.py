38dc906aa05aefb90bb7a2f1c0a7f0ea7397ff16
jrj92280 <jrj92280@gmail.com>
2018-05-29 15:48:24 -0400

    Add bottles route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
+from flask import request
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Hello, World! " + request.args.get('name', "Jason")
+
+
+@app.route('/bottles')
+def bottles():
+    _ = ''
+    for count in range(100, 0, -1):
+        _ += str(count) + ' bottles of beer on the wall<br/>'
+    return "<html><h5>Hello, World! " + \
+           request.args.get('name', "Jason") + "</h5>" + _ + "</html>"
